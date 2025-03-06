d2562b14883126943d6c787522a3b21ba73f06c1
JudicaelFelix <yuya12@orange.fr>
2025-03-06 09:45:44 +0100

    suite tp trop bien

--- src/tests_fonctionnnels/TestSabot.java
+++ src/tests_fonctionnnels/TestSabot.java
@@ -1,5 +1,12 @@
 package tests_fonctionnnels;
+import Jeu.*;
+import cartes.JeuDeCartes;
 
 public class TestSabot {
-
+	
+	public static void main(String[] args) {
+		
+		Sabot sabot = new Sabot(Carte[]);
+		
+	}
 }
