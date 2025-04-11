409bf1bbc99118b458aa5b8e11fda7600cd968ad
JudicaelFelix <yuya12@orange.fr>
2025-04-11 11:58:41 +0200

    recup tp

--- src/utils/GestionCartes.java
+++ src/utils/GestionCartes.java
@@ -3,6 +3,7 @@ import cartes.*;
 import jeu.*;
 import tests_fonctionnnels.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -12,6 +13,23 @@ public class GestionCartes {
 	private <E> E extrairev1(List<E> liste) {
 		Random randomNumbers = new Random();
 		return liste.get(randomNumbers.nextInt(liste.size()));
-		
 	}
+	
+	private <E> List <E> melanger(List<E> liste) {
+		List<E> newlist = new ArrayList<>();
+		for(int i =0; i< liste.size(); i++) {
+			newlist.add(extrairev1(liste));
+		}
+		return newlist;
+	}
+	
+	private <E> boolean verifierMelange(List<E> liste, List<E> liste2) {
+		boolean v = true;
+		if(liste.size() == liste2.size()) {
+			for(int i = 0; i< liste2.size(); i++) {
+				v = ((liste2.get(i)).frequency == liste.get(i).frequency)
+			}
+		}
+	}
+	
 }
