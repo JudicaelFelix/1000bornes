7397e9c6c3adf9724968a33a6a6f84a7a33038f9
JudicaelFelix <yuya12@orange.fr>
2025-04-17 09:13:36 +0200

    suite

--- src/utils/GestionCartes.java
+++ src/utils/GestionCartes.java
@@ -28,10 +28,49 @@ public class GestionCartes {
 		boolean v = true;
 		if(liste.size() == liste2.size()) {
 			for(int i = 0; i< liste2.size(); i++) {
-				v = v && (((liste2.frequency(liste2.get(i)) == liste.get(i).frequency));
+				v = v && ((Collections.frequency(liste2, liste2.get(i)) == Collections.frequency(liste, liste2.get(i))));
 			}
 		}
 		return v;
 	}
 	
 }
+	private <E> List <E> rassembler(List<E> liste){
+		List<E> newlist = new ArrayList<>();
+		for(int i = 0 ; i < liste.size(); i++) {
+			if(!newlist.contains(liste.get(i))) {
+				for(int j = 0; j< Collections.frequency(liste, liste.get(i)); j++) {
+					newlist.add(liste.get(i));
+				}
+			}
+		}
+		return newlist;
+	}
+	
+	private <E> boolean verifierRassemblement(List<E> liste) {
+		boolean v = true;
+		if(!)
+		
+}
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
